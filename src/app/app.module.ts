00cd2eafc46303af39cf2301b749f4f95e68fc50
Vaibhav Padalia <vaibhavpadalia07@gmail.com>
2018-04-08 20:40:21 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule, MatCardModule } from '@angular/material';
 
 
 import { AppComponent } from './app.component';
+import { ImgCardComponent } from './img-card/img-card.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ImgCardComponent],
+  imports: [BrowserModule, MatToolbarModule, MatCardModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
